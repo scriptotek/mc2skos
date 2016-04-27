scriptotek/mc2skos

commit 1818b32aeaab3dfc472410e66e55cdcb77108d9e

Remove T prefix in table notation
since the prefix varies from language edition to language edition

mc2skos/mc2skos.py
@@ -326,7 +326,7 @@ def process_record(rec, nsmap, namespace, skos_scheme, same_as, include_indexter
                 elif sf.get('code') == 'r':    # Root number
                     rootno = sf.text
                 elif sf.get('code') == 'z':    # Table identification
-                    table = 'T{}--'.format(sf.text)
+                    table = '{}--'.format(sf.text)
                 # elif sf.get('code') == 't':    # Digits added from internal subarrangement or add table
                 #     components.append(sf.text)
                 elif sf.get('code') == 's':  # Digits added from classification number in schedule or external table
@@ -366,7 +366,7 @@ def get_classno(node, nsmap):
         code = sf.get('code')
 
         if code == 'z':
-            table = 'T{}--'.format(sf.text)
+            table = '{}--'.format(sf.text)
         elif code == 'y':
             if sf.text == '1':
                 addtable = ':'
@@ -405,7 +405,7 @@ def get_parent(node, nsmap):
         code = sf.get('code')
 
         if code == 'z':
-            table = 'T{}--'.format(sf.text)
+            table = '{}--'.format(sf.text)
         elif code == 'y':
             if sf.text == '1':
                 addtable = ':'

commit a5d07eead5a84f9f4f0863d716f1c215b8153955

Fix ordering of table classes
that broke in 1818b32aeaab3dfc472410e66e55cdcb77108d9e

mc2skos/mc2skos.py
@@ -527,7 +527,7 @@ def main():
     s = OrderedTurtleSerializer(g)
     s.sorters = {
       'http://dewey.info/class/([0-9.]+)': lambda x: float(x[0]),
-      'http://dewey.info/class/T([0-9])\-\-([0-9]+)': lambda x: 1000. + int(x[0]) + float('.' + x[1])
+      'http://dewey.info/class/([0-9])\-\-([0-9]+)': lambda x: 1000. + int(x[0]) + float('.' + x[1])
     }
     s.serialize(open(out_file, 'wb'))
     logger.info('Wrote RDF: %s', out_file)