3afec2a68f0ed757a432152eb7b76be77afe6bdc
Dan Michael O. Heggø <danmichaelo@gmail.com>
2016-05-19 14:25:33 +0200

    Parameterize example tests

--- tests/test_process_examples.py
+++ tests/test_process_examples.py
@@ -2,6 +2,7 @@
 import unittest
 import pytest
 import os
+import glob
 import re
 from lxml import etree
 from mc2skos.mc2skos import process_record
@@ -9,56 +10,52 @@ from rdflib.namespace import RDF, SKOS, Namespace
 from rdflib import URIRef, Literal, Graph
 
 
-def get_records(marcfile):
-    for _, record in etree.iterparse(marcfile, tag='{http://www.loc.gov/MARC21/slim}record'):
+def get_records(filename):
+    for _, record in etree.iterparse(filename, tag='{http://www.loc.gov/MARC21/slim}record'):
         yield record
         record.clear()
 
 
-class TestProcessExamples(unittest.TestCase):
+@pytest.fixture(params=glob.glob('examples/*.xml'))
+def marc_file(request):
+    return request.param
 
-    def setUp(self):
-        self.graph = Graph()
-        self.nsmap = {'mx': 'http://www.loc.gov/MARC21/slim'}
-        self.ns = Namespace('http://test/')
 
-    def testExamples(self):
-        pattern = re.compile('^(?P<name>(?P<scheme>[a-z]+\d{2})(?P<lang>[a-z]+)-(?P<notation>((?P<table>\d+)--)?[\d.]+-?[\d.]*))\.xml$')
+def test_example(marc_file):
 
-        for marcfile in os.listdir('examples'):
-            match = pattern.match(marcfile)
-            if not match:
-                continue
+    graph = Graph()
+    nsmap = {'mx': 'http://www.loc.gov/MARC21/slim'}
+    ns = Namespace('http://test/')
 
-            name = match.group('name')
-            notation = match.group('notation')
-            table = match.group('table')
+    print(marc_file)
+    match = re.match('^(?P<name>examples/(?P<scheme>[a-z]+\d{2})(?P<lang>[a-z]+)-(?P<notation>((?P<table>\d+)--)?[\d.]+-?[\d.]*))\.xml$', marc_file)
+    assert match is not None
 
-            marcfile = 'examples/' + marcfile
-            # print marcfile
-            rdffile = 'examples/' + name + '.ttl'
+    name = match.group('name')
+    notation = match.group('notation')
+    table = match.group('table')
 
-            graph = Graph()
-            for record in get_records(marcfile):
-                process_record(graph, record, self.nsmap, self.ns, None, None)
-            # print graph.serialize(format='turtle')
+    rdf_file = marc_file[:marc_file.rindex('.')] + '.ttl'
 
-            expect = Graph()
-            uri = URIRef(u'http://test/' + notation)
-            expect.add((uri, RDF.type, SKOS.Concept))
-            if table:
-                notation = "T" + notation
-            expect.add((uri, SKOS.notation, Literal(notation)))
+    graph = Graph()
+    for record in get_records(marc_file):
+        process_record(graph, record, nsmap, ns, None, None)
 
-            if os.path.isfile(rdffile):
-                expect.parse(rdffile, format='turtle')
-            else:
-                if len(graph) > 0:
-                    graph.namespace_manager.bind('skos', URIRef('http://www.w3.org/2004/02/skos/core#'))
-                    graph.serialize(destination=rdffile, format='turtle')
+    expect = Graph()
+    uri = URIRef(u'http://test/' + notation)
+    expect.add((uri, RDF.type, SKOS.Concept))
+    if table:
+        notation = "T" + notation
+    expect.add((uri, SKOS.notation, Literal(notation)))
 
-            for triple in expect:
-                assert triple in graph
+    if os.path.isfile(rdf_file):
+        expect.parse(rdf_file, format='turtle')
+    elif len(graph) > 0:
+        graph.namespace_manager.bind('skos', URIRef('http://www.w3.org/2004/02/skos/core#'))
+        graph.serialize(destination=rdf_file, format='turtle')
+
+    for triple in expect:
+        assert triple in graph
 
 if __name__ == '__main__':
     unittest.main()
