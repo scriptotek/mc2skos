7b2f52ea874a5c40a7ee60032ecfd52da624dfe5
Jakob Voss <voss@gbv.de>
2017-01-18 14:56:44 +0100

    Add example of Basisklassifikation (#13)

--- tests/test_process_examples.py
+++ tests/test_process_examples.py
@@ -2,6 +2,7 @@
 import unittest
 import pytest
 import os
+import sys
 import glob
 import re
 from lxml import etree
@@ -10,35 +11,57 @@ from rdflib.namespace import RDF, SKOS, Namespace
 from rdflib import URIRef, Literal, Graph
 
 
-def get_records(filename):
-    for _, record in etree.iterparse(filename, tag='{http://www.loc.gov/MARC21/slim}record'):
-        yield record
-        record.clear()
+class MarcFile:
+    record_tag = '{http://www.loc.gov/MARC21/slim}record'
+
+    def __init__(self, name):
+        self.name = name
+
+    def records(self):
+        for _, record in etree.iterparse(self.name, tag=MarcFile.record_tag):
+            yield record
+            record.clear()
+
+    def processed_records(self, **options):
+        graph = Graph()
+        for record in self.records():
+            process_record(graph, record, **options)
+        return graph
 
 
-@pytest.fixture(params=glob.glob('examples/*.xml'))
-def marc_file(request):
-    return request.param
+def examples(prefix, pattern):
+
+    pattern = '^(examples/%s%s)\.xml$' % (prefix, pattern)
+    files = glob.glob('examples/%s*.xml' % prefix)
+
+    return [(MarcFile(f), re.match(pattern, f)) for f in files]
 
 
+def check_rdf(graph, expect, rdf_file):
+    graph.namespace_manager.bind('skos', URIRef('http://www.w3.org/2004/02/skos/core#'))
+
+    if os.path.isfile(rdf_file):
+        expect.parse(rdf_file, format='turtle')
+    elif len(graph) > 0:
+        graph.serialize(destination=rdf_file, format='turtle')
+
+    # graph.serialize(destination=sys.stdout, format='turtle')
+
+    for triple in expect:
+        assert triple in graph
+
+
+@pytest.mark.parametrize('marc_file',
+                         examples('ddc',
+                                  '(?P<edition>\d{2})(?P<lang>[a-z]+)-'
+                                  '(?P<notation>((?P<table>\d+)--)?[\d.]+-?[\d.]*)'))
 def test_ddc_example(marc_file):
+    marc, match = tuple(marc_file)
 
-    graph = Graph()
-    nsmap = {'mx': 'http://www.loc.gov/MARC21/slim'}
-
-    match = re.match('^(?P<name>examples/ddc(?P<edition>\d{2})(?P<lang>[a-z]+)-(?P<notation>((?P<table>\d+)--)?[\d.]+-?[\d.]*))\.xml$', marc_file)
-    assert match is not None
-
-    name = match.group('name')
     edition = match.group('edition')
     notation = match.group('notation')
     table = match.group('table')
-
-    rdf_file = marc_file[:marc_file.rindex('.')] + '.ttl'
-
-    graph = Graph()
-    for record in get_records(marc_file):
-        process_record(graph, record)
+    rdf_file = match.group(1) + '.ttl'
 
     expect = Graph()
     uri = URIRef(u'http://dewey.info/class/' + notation + '/e' + edition + '/')
@@ -47,14 +70,24 @@ def test_ddc_example(marc_file):
         notation = "T" + notation
     expect.add((uri, SKOS.notation, Literal(notation)))
 
-    if os.path.isfile(rdf_file):
-        expect.parse(rdf_file, format='turtle')
-    elif len(graph) > 0:
-        graph.namespace_manager.bind('skos', URIRef('http://www.w3.org/2004/02/skos/core#'))
-        graph.serialize(destination=rdf_file, format='turtle')
+    graph = marc.processed_records()
+    check_rdf(graph, expect, rdf_file)
 
-    for triple in expect:
-        assert triple in graph
+
+@pytest.mark.parametrize('marc_file', examples('bk', '-(?P<notation>[0-9.]+)'))
+def test_bk_example(marc_file):
+    marc, match = tuple(marc_file)
+
+    notation = match.group('notation')
+    rdf_file = match.group(1) + '.ttl'
+
+    expect = Graph()
+    uri = URIRef(u'http://uri.gbv.de/terminology/bk/' + notation)
+    expect.add((uri, RDF.type, SKOS.Concept))
+
+    graph = marc.processed_records(include_indexterms=True)
+    check_rdf(graph, expect, rdf_file)
+
 
 if __name__ == '__main__':
     unittest.main()
