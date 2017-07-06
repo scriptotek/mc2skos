6c86b514364ae50e20ea66e7c4ebe5d03eec132a
Dan Michael O. Heggø <danmichaelo@gmail.com>
2017-07-06 20:08:02 +0200

    Fix Python 3 support

--- mc2skos/record.py
+++ mc2skos/record.py
@@ -55,6 +55,13 @@ def is_uri(value):
     return value.startswith('http://') or value.startswith('https://')
 
 
+def is_str(obj):
+    try:
+        return isinstance(obj, basestring)  # Python 2.x
+    except NameError:
+        return isinstance(obj, str)  # Python 3.x
+
+
 class InvalidRecordError(RuntimeError):
     pass
 
@@ -115,7 +122,7 @@ class ConceptScheme(object):
         if self.config is None:
             return
 
-        if isinstance(self.config, basestring):
+        if is_str(self.config):
             uri_template = self.config
         else:
             if uri_type not in self.config:
